dc711115c9c980433df8d92d2c49468f950211d8
Clement Miao <clementmiao@gmail.com>
2018-04-02 22:33:24 -0700

    dynamic focus menu, start of custom focus

--- Samadhi/StatusMenuController.swift
+++ Samadhi/StatusMenuController.swift
@@ -12,12 +12,18 @@ class StatusMenuController: NSObject {
     @IBOutlet weak var statusMenu: NSMenu!
 
     let statusItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
+    
+    let defaultTime = 300
 
     override func awakeFromNib() {
         let icon = NSImage(named: NSImage.Name(rawValue: "statusIcon"))
         icon?.isTemplate = true // best for dark mode
         statusItem.image = icon
         statusItem.menu = statusMenu
+        if let focusMenuItem = self.statusMenu.item(withTitle: "Focus") {
+//            focusMenuItem.title = String(format: "Focus for %@ minutes", defaultTime)
+            focusMenuItem.title = "Focus for " + String(defaultTime/60) + " minutes"
+        }
     }
 
     @IBAction func focusClicked(_ sender: NSMenuItem) {
